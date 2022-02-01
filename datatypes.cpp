06af49b7c7492e0cceb9e075130ccf8c7789ee82
Aakash <aakash@codewithaakash.com>
2022-02-02 00:10:40 +0530

    lecture 1 updated

---
+++ datatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a= 5;
+    double b=5.6;
+    char c= 'a';
+    bool d= true;
+    long int e = 543332;
+    long double f= 67.77667;
+    short int g= 6.7;
+    signed int h = 67;
+    signed char i= 'f';
+    
+
+
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(c) << endl;
+    cout << sizeof(d) << endl;
+    cout << sizeof(e) << endl;
+    cout << sizeof(f) << endl;
+    cout << sizeof(g) << endl;
+    cout << sizeof(h) << endl;
+}
